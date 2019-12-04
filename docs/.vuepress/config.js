a77a104e9295c76722d973fc7fedcc2110409454
xugaoyi <894072666@qq.com>
2019-12-04 14:37:33 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   title: 'Evan Blog', // 标题
   description: 'Evan Blog 个人博客，基于vuepress构建，专注前端学习与总结', // 描述，以 <meta> 标签渲染到当前页面的 HTML 中
   // process.env.VUEPRESS_BASE 在package.json生成
-  base: process.env.VUEPRESS_BASE || '/', // 基础路径，默认'/',如果放到github pages要改成仓库名
+ // base: process.env.VUEPRESS_BASE || '/', // 基础路径，默认'/',如果放到github pages要改成仓库名
   head: [ // 注入到当前页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', { rel: 'icon', href: '/img/logo.png' }] //favicons，资源放在public文件夹
   ],
