e5c79b58838bae2aa2b8a03c4aadbc035cdce155
xugaoyi <894072666@qq.com>
2019-12-07 17:38:57 +0800

    init

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+#npm run docs:build
+npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+echo 'www.evanblogweb.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+git push -f git@github.com:xugaoyi/evanblog.git master:gh-pages
+
+cd -
