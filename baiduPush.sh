045d184cbf6a1c3ac2ec68b221b339e1449b946d
xugaoyi <894072666@qq.com>
2020-03-07 12:47:24 +0800

    优化

--- baiduPush.sh
+++ baiduPush.sh
@@ -3,8 +3,6 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-node utils/baiduPush.js # 生成所有链接文件
-
 # 百度链接推送
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://xugaoyi.com&token=T5PEAzhGaPNbjQ2X"
 
--- utils/baiduPush.js
+++ utils/baiduPush.js
@@ -3,11 +3,16 @@
  */
 const fs = require('fs');
 const path = require('path');
+const logger = require('tracer').colorConsole();
 const matter = require('gray-matter'); // FrontMatter解析器 https://github.com/jonschlinkert/gray-matter
 const readFileList = require('./modules/readFileList');
 const urlsRoot = path.join(__dirname, '..', 'urls.txt'); // 百度链接推送文件
+const DOMAIN = process.argv.splice(2)[0]; // 获取命令行传入的参数
 
-const DOMAIN = 'https://xugaoyi.com'
+if (!DOMAIN) {
+  logger.error('请在运行此文件时指定一个你要进行百度推送的域名参数，例：node utils/baiduPush.js https://xugaoyi.com')
+  return
+}
 
 main();
 
@@ -19,7 +24,12 @@ function main() {
   const files = readFileList(); // 读取所有md文件数据
 
   files.forEach( file => {
-    const { data } = matter(fs.readFileSync(file.filePath, 'utf8'));
-    data.permalink && fs.appendFileSync(urlsRoot, `\r\n${DOMAIN}${data.permalink}/`);
+    const { data } = matter(fs.readFileSync(file.filePath, 'utf8')); 
+
+    if (data.permalink) {
+      const link = `\r\n${DOMAIN}${data.permalink}/`;
+      console.log(link)
+      fs.appendFileSync(urlsRoot, link);
+    }
   })
 }
