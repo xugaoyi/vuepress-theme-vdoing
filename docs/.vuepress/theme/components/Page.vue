cc1b0effa1a5d7a0e839f354b6db21a677824e71
xugaoyi <894072666@qq.com>
2020-03-12 17:17:16 +0800

    更新：时间轴页  & README 等

--- docs/.vuepress/theme/components/Page.vue
+++ docs/.vuepress/theme/components/Page.vue
@@ -20,9 +20,10 @@ import PageEdit from '@theme/components/PageEdit.vue'
 import PageNav from '@theme/components/PageNav.vue'
 import Catalogue from './Catalogue.vue'
 import Article from './Article.vue'
+import Timeline from './Timeline.vue'
 
 export default {
-  components: { PageEdit, PageNav, Catalogue, Article},
+  components: { PageEdit, PageNav, Catalogue, Article, Timeline},
   props: ['sidebarItems'],
   computed: {
     pageComponent () {
