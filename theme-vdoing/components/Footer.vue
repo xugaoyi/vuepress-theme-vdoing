54a4c264935b4ab854537e3f8e11de4984e2d4fb
Evan Xu <894072666@qq.com>
2020-04-17 00:12:21 +0800

    Update Footer.vue

--- theme-vdoing/components/Footer.vue
+++ theme-vdoing/components/Footer.vue
@@ -12,7 +12,7 @@
       </a>
     </div>
     <template v-if="footer">
-      <!--声明: 除非你对主题改动特别大，否则请不要删除或更改主题链接，尊重vdoing作者版权。-->
+      <!--vdoing主题遵循MIT协议，完全开源且免费。如果你对主题的修改并不大，希望你可以保留主题的链接。-->
       <a href="https://github.com/xugaoyi/vuepress-theme-vdoing" target="_blank" title="本站主题">♔♕Vdoing | </a>
       Copyright © {{ footer.createYear }}-{{ new Date().getFullYear() }}
       <span v-html="footer.copyrightInfo"></span>
