ac50d847bd5223d881b89eade7481e15653e8201
xugaoyi <894072666@qq.com>
2020-04-21 18:26:25 +0800

    优化代码

--- utils/baiduPush.js
+++ utils/baiduPush.js
@@ -3,14 +3,14 @@
  */
 const fs = require('fs');
 const path = require('path');
-const logger = require('tracer').colorConsole();
+const chalk = require('chalk')
 const matter = require('gray-matter'); // FrontMatter解析器 https://github.com/jonschlinkert/gray-matter
 const readFileList = require('./modules/readFileList');
 const urlsRoot = path.join(__dirname, '..', 'urls.txt'); // 百度链接推送文件
 const DOMAIN = process.argv.splice(2)[0]; // 获取命令行传入的参数
 
 if (!DOMAIN) {
-  logger.error('请在运行此文件时指定一个你要进行百度推送的域名参数，例：node utils/baiduPush.js https://xugaoyi.com')
+  console.log(chalk.red('请在运行此文件时指定一个你要进行百度推送的域名参数，例：node utils/baiduPush.js https://xugaoyi.com'))
   return
 }
 
