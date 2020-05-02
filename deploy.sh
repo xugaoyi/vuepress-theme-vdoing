4cb61e0e198807086f787c650cd612232a5173cc
xugaoyi <894072666@qq.com>
2020-05-02 22:02:19 +0800

    分类开发

--- deploy.sh
+++ deploy.sh
@@ -26,12 +26,12 @@ git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github
 
 # deploy to coding
-echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  # codingUrl=git@e.coding.net:xgy/xgy.git
-else
-  # codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-fi
+# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#   codingUrl=git@e.coding.net:xgy/xgy.git
+# else
+#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
+# fi
 # git add -A
 # git commit -m "${msg}"
 # git push -f $codingUrl master # 推送到coding
