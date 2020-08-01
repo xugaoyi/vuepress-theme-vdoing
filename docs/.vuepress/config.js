adaf01d7154821d0a5449326c1b739e9ab2bcb4b
xugaoyi <894072666@qq.com>
2020-08-01 14:31:12 +0800

    添加作者

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -58,7 +58,7 @@ module.exports = {
     sidebar: 'structuring', // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
     
     author: { // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
-      name: 'Evan Xu', // 必需
+      name: 'xugaoyi', // 必需
       link: 'https://github.com/xugaoyi' // 可选的
     },
     blogger:{ // 博主信息，显示在首页侧边栏
