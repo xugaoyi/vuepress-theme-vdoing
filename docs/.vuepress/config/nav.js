1a94d6b86c8b09b73e3a62e5ffcbb0cadbc2ad21
Evan Xu <894072666@qq.com>
2020-08-25 23:59:49 +0800

    Update nav.js

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -2,10 +2,10 @@ module.exports = [
   {text: '首页', link: '/'},
   {
     text: '前端',
-    link: '/web/',  //目录页，vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    link: '/web/',  //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       {text: '前端文章', items: [
-        {text: 'JavaScript', link: '/pages/8143cc480faf9a11/'}, // 注意link结尾有斜杠和没有斜杠的区别
+        {text: 'JavaScript', link: '/pages/8143cc480faf9a11/'}, // 这些link是在相应md文件定义的永久链接。另外，注意结尾是有斜杠的
         {text: 'Vue', link: '/pages/802a1ca6f7b71c59/'},
       ]},
       {text: '学习笔记', items:[
