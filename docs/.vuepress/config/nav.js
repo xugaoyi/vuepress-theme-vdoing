9eb68c2b53407aacbd4dd369e150e3296832e266
xugaoyi <894072666@qq.com>
2020-11-09 11:12:24 +0800

    添加： 文章-2分钟规则

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -19,7 +19,7 @@ module.exports = [
     ]
   },
   {
-    text: '页面', 
+    text: '页面',
     link: '/ui/',
     items: [
       {text: 'HTML', link: '/pages/8309a5b876fc95e3/'},
@@ -37,12 +37,13 @@ module.exports = [
     ]
   },
   {
-    text: '更多', 
+    text: '更多',
     link: '/more/',
     items: [
       {text: '学习', link: '/pages/f2a556/'},
       {text: '面试', link: '/pages/aea6571b7a8bae86/'},
-      {text: '心情杂货', link: '/pages/2d615df9a36a98ed/'},
+      { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
+      { text: '实用技巧', link: '/pages/baaa02/' },
       {text: '友情链接', link: '/friends/'},
     ]
   },
