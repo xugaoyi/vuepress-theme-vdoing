efa8a58e14391aa6b283573b872fa871c5b10273
xugaoyi <894072666@qq.com>
2020-12-15 23:21:20 +0800

    google ads test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,6 @@ module.exports = {
   description:
     'web前端技术博客,简洁至上,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github等技术文章。', // 描述,以 <meta> 标签渲染到页面html中
   // base: '/', // '/<github仓库名>/'， 默认'/'
-  // <script data-ad-client="ca-pub-7828333725993554" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
   head: [
     // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
