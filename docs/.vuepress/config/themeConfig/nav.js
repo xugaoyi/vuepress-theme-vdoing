108e7973bf428e67be58bbbd4bfc8a1d0114bd4c
xugaoyi <894072666@qq.com>
2021-03-25 20:24:44 +0800

    blog: 添加React学习笔记

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -20,6 +20,7 @@ module.exports = [
           { text: '《JavaScript高级程序设计》笔记', link: '/note/js/' },
           { text: '《ES6 教程》笔记', link: '/note/es6/' },
           { text: '《Vue》笔记', link: '/note/vue/' },
+          { text: '《React》笔记', link: '/note/react/' },
           {
             text: '《TypeScript 从零实现 axios》',
             link: '/note/typescript-axios/',
