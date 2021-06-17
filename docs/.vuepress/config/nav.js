5d075fec98245fce6c819702d9e9b671a38d2bf6
xugaoyi <894072666@qq.com>
2021-06-17 19:57:50 +0800

    blog: 更新`关于`

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -74,11 +74,11 @@ module.exports = [
   {
     text: '收藏',
     link: '/pages/beb6c0bd8a66cea6/',
-    items: [
-      { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-      { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-      { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-    ],
+    // items: [
+    //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
+    //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
+    //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
+    // ],
   },
   {
     text: '索引',
