d7b26517854863fa65aefae30f8216b2cc764d5c
Administrator <admin@example.com>
2021-09-07 14:50:45 +0800

    改造博客

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:lsqiong # 推送到github gh-pages分支
+git push -f $githubUrl master:lsqiong # 推送到github lsqiong分支
 
 # deploy to coding pages
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
