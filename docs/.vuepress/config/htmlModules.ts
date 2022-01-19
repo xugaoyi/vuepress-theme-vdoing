68a786305bad4a0685d7d7031bc928c107e5ed81
xugaoyi <894072666@qq.com>
2022-01-19 11:26:09 +0800

    fix: htmlModules文件改为ts

--- docs/.vuepress/config/htmlModules.ts
+++ docs/.vuepress/config/htmlModules.ts
@@ -1,3 +1,5 @@
+import { VdoingThemeConfig } from 'vuepress-theme-vdoing/types'
+
 /** 插入自定义html模块 (可用于插入广告模块等)
  * {
  *   homeSidebarB: htmlString, 首页侧边栏底部
@@ -17,7 +19,7 @@
  * ①注：在.md文件front matter配置`article: false`的页面是自定义页，未配置的默认是文章页（首页除外）。
  * ②注：windowLB 和 windowRB：1.展示区块最大宽高200px*400px。2.请给自定义元素定一个不超过200px*400px的宽高。3.在屏幕宽度小于960px时无论如何都不会显示。
  */
-const htmlModule = {
+const htmlModule: VdoingThemeConfig['htmlModules'] = {
   homeSidebarB:
     `<div style="padding: 0.95rem">
     <p style="
