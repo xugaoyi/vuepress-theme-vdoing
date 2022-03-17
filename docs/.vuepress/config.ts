b9cf8362edb35e40843e8af9b05f294ea9cf7d7d
xugaoyi <894072666@qq.com>
2022-03-17 23:12:03 +0800

    test: 全文搜索

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -235,8 +235,8 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       },
     ],
 
-    // 全文搜索。 ⚠️注意：此插件会在打开网站时一次性加载所有页面相应的js文件用于搜索，导致初次访问网站加载变慢。如在意初次访问速度的话可以不使用此插件！（推荐：vuepress-plugin-thirdparty-search）
-    // 'fulltext-search',
+    // 全文搜索。 ⚠️注意：此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。如在意初次访问速度的话可以不使用此插件！（推荐：vuepress-plugin-thirdparty-search）
+    'fulltext-search',
 
     // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数）
     // 'thirdparty-search': {
