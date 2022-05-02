aa0a73d358f1602ff459f7ef567c1d1399e1685c
xugaoyi <894072666@qq.com>
2022-05-02 12:20:58 +0800

    chore: 更换搜索插件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -244,34 +244,34 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     ],
 
     // 全文搜索。 ⚠️注意：此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。如在意初次访问速度的话可以不使用此插件！（推荐：vuepress-plugin-thirdparty-search）
-    'fulltext-search',
+    // 'fulltext-search',
 
     // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数）
-    // 'thirdparty-search': {
-    //   thirdparty: [
-    //     {
-    //       title: '在MDN中搜索',
-    //       frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-    //       behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
-    //     },
-    //     {
-    //       title: '在Runoob中搜索',
-    //       frontUrl: 'https://www.runoob.com/?s=',
-    //     },
-    //     {
-    //       title: '在Vue API中搜索',
-    //       frontUrl: 'https://cn.vuejs.org/v2/api/#',
-    //     },
-    //     {
-    //       title: '在Bing中搜索',
-    //       frontUrl: 'https://cn.bing.com/search?q=',
-    //     },
-    //     {
-    //       title: '通过百度搜索本站的',
-    //       frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20',
-    //     },
-    //   ],
-    // },
+    'thirdparty-search': {
+      thirdparty: [
+        {
+          title: '在MDN中搜索',
+          frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
+          behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
+        },
+        {
+          title: '在Runoob中搜索',
+          frontUrl: 'https://www.runoob.com/?s=',
+        },
+        {
+          title: '在Vue API中搜索',
+          frontUrl: 'https://cn.vuejs.org/v2/api/#',
+        },
+        {
+          title: '在Bing中搜索',
+          frontUrl: 'https://cn.bing.com/search?q=',
+        },
+        {
+          title: '通过百度搜索本站的',
+          frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20',
+        },
+      ],
+    },
 
     [
       'one-click-copy', // 代码块复制按钮
