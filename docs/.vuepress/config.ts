7c5ab13a07a68f751752737c4e91a2465838e189
xugaoyi <894072666@qq.com>
2022-06-02 10:39:03 +0800

    chore: 添加docsBranch配置说明

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -120,6 +120,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
     docsDir: 'docs', // 编辑的文件夹
+    // docsBranch: 'master', // 编辑的文件所在分支，默认master。 注意：如果你的分支是main则修改为main
     editLinks: true, // 启用编辑
     editLinkText: '编辑',
 
