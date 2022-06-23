07eefcca838bee2399d5354d329e6240503edd33
xugaoyi <894072666@qq.com>
2022-06-23 17:04:33 +0800

    feat: v1.12.4

--- vdoing/components/Home.vue
+++ vdoing/components/Home.vue
@@ -224,7 +224,7 @@ export default {
         } else {
           return 'background: var(--mainBg);color: var(--textColor)'
         }
-      } else if (bannerBg.indexOf('background') > -1) { // 自定义背景样式
+      } else if (bannerBg.indexOf('background:') > -1) { // 自定义背景样式
         return bannerBg
       } else if (bannerBg.indexOf('.') > -1) { // 大图
         return `background: url(${this.$withBase(bannerBg)}) center center / cover no-repeat`
